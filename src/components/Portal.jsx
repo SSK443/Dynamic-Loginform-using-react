b6bef651cbc14b01a5575fc6a7bcbebb18dfdd5a
SURAJ S KUMAR <143587320+SSK443@users.noreply.github.com>
2024-12-12 15:28:36 +0530

    Your commit message here

--- src/components/Portal.jsx
+++ src/components/Portal.jsx
@@ -8,7 +8,7 @@ function Portal() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 flex flex-col items-center justify-center text-white">
       <div className="bg-white text-gray-800 p-10 rounded-lg shadow-lg w-11/12 md:w-1/2 lg:w-1/3">
-        <h1 className="md:text-4xl mb-4 md:font-bold md:text-center text-justify text-lg">Welcome, {email}</h1>
+        <h1 className="md:text-4xl mb-4 font-bold md:text-center text-justify text-xl">Welcome, {email}</h1>
         <p className="text-center mb-6 ">We're glad to have you here!</p>
         <Link to="/">
           <button className="w-full bg-blue-600 hover:bg-blue-700 transition duration-300 text-white py-3 rounded-lg font-semibold">
